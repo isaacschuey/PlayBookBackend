ee7c721d99b3b0ed5bcb2c63a9f66bac4d6dab1d
Isaac <isaac.d.schumacher@ad.ndsu.edu>
2026-04-01 16:35:08 -0500

    Remove pycache and venv from repository

--- playbook_api/urls.py
+++ playbook_api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hockey/<str:date>/', views.hockey_games),
+    path('games/nhl', views.nhl_games),
+    path('games/mlb', views.mlb_games),
+    path('games/nba', views.nba_games)
 ]
--- playbook_api/views.py
+++ playbook_api/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
-
-# Create your views here.
 import requests
 from rest_framework.decorators import api_view
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 @api_view(['GET'])
-def hockey_games(request, date):
-    url = f'https://api-web.nhle.com/v1/score/{date}'
+def mlb_games(req: Request) -> Response:
+    date = req.query_params.get("date")
+    url = f"https://statsapi.mlb.com/api/v1/schedule?sportId=1&hydrate=team,stats,game,venue&language=en&date={date}"
+    response = requests.get(url)
+    return Response(response.json())
+
+@api_view(['GET'])
+def nhl_games(req: Request) -> Response:
+    date = req.query_params.get("date")
+    url = f"https://api-web.nhle.com/v1/score/{date}"
+    response = requests.get(url)
+    return Response(response.json())
+
+@api_view(['GET'])
+def nba_games(req: Request) -> Response:
+    url = "https://cdn.nba.com/static/json/staticData/scheduleLeagueV2_1.json"
     response = requests.get(url)
     return Response(response.json())
